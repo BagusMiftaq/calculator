c754fb876265726e2cca82fd8dee39bd4f557a59
BagusMiftaq <bagusmiftaq@gmail.com>
2022-05-25 21:27:47 +0700

    third commit

--- assets/kalk.js
+++ assets/kalk.js
@@ -1,33 +1,96 @@
 const calculator = {
-    displayNumber: '0',
-    operator : null,
-    firsNumber : null,
-    waitingForSecondNumber : false
+  displayNumber: "0",
+  operator: null,
+  firsNumber: null,
+  waitingForSecondNumber: false,
 };
 
 function updateDisplay() {
-    document.querySelector("#displayNumber").innerText = calculator.displayNumber;
+  document.querySelector("#displayNumber").innerText = calculator.displayNumber;
 }
 
-function clearCalculator(){
-    calculator.displayNumber = '0';
-    calculator.operator = null;
-    calculator.firsNumber = null;
-    calculator.waitingForSecondNumber = false;
+function clearCalculator() {
+  calculator.displayNumber = "0";
+  calculator.operator = null;
+  calculator.firsNumber = null;
+  calculator.waitingForSecondNumber = false;
 }
 
 function inputDigit(digit) {
-    calculator.displayNumber += digit;
+    if (calculator.displayNumber === "0") {
+      calculator.displayNumber = digit;
+    } else {
+      calculator.displayNumber += digit;
+    }
+  }
+
+function inverseNumber() {
+    if (calculator.displayNumber === '0') {
+        return;
+    }
+
+    calculator.displayNumber = calculator.displayNumber * -1;
+}
+
+function handleOperator(operator){
+    if (!calculator.waitingForSecondNumber) {
+        calculator.operator = operator;
+        calculator.waitingForSecondNumber = true;
+        calculator.firsNumber = calculator.displayNumber;
+
+        //mengatur ulang nilai display number supaya tombol selanjutnya dimulai dari angka pertama lagi
+        calculator.displayNumber = '0';
+    } else {
+        alert('Operator sudah ditetapkan')
+    }
+}
+
+function performCalculation(){
+    if (calculator.firsNumber == null || calculator.operator == null){
+        alert("Anda belum menetapkan operator");
+        return;
+    }
+
+    let result = 0;
+    if (calculator.operator === "+") {
+        result = parseInt(calculator.firsNumber) + parseInt(calculator.displayNumber);
+    } else {
+        result = parseInt(calculator.firsNumber) - parseInt(calculator.displayNumber);
+    }
+
+    calculator.displayNumber = result;
 }
 
 const buttons = document.querySelectorAll(".button");
 for (let button of buttons) {
-    button.addEventListener('click', function(event) {
-        // menampilkan halaman yang di klik
-        const target = event.target;
+  button.addEventListener("click", function (event) {
+    // menampilkan halaman yang di klik
+    const target = event.target;
 
-        inputDigit(target.innerText);
-        updateDisplay();
+    if (target.classList.contains("clear")) {
+      clearCalculator();
+      updateDisplay();
+      return;
+    }
 
-    });
+    if (target.classList.contains("negative")) {
+      inverseNumber();
+      updateDisplay();
+      return;
+    }
+
+    if (target.classList.contains("equals")) {
+      performCalculation();
+      updateDisplay();
+      return;
+    }
+
+    if (target.classList.contains("operator")) {
+      handleOperator(target.innerText);
+      return;
+    }
+
+    inputDigit(target.innerText);
+    updateDisplay();
+  });
 }
